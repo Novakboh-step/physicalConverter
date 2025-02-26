ba937ff90cc35d90a881392b09523dbabe925d21
Novakboh-step <novak_dv82@student.itstep.org>
2025-02-26 11:41:20 +0200

    Added GUI

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 from DistanceConverter import DistanceConverter
 from MassConverter import MassConverter
 from TimeConverter import TimeConverter
+from tkinter import *
+from tkinter import ttk
 
-def convert(c):
-    number = float(input("Enter number: "))
-    unit1 = input("Enter unit 1: ")
-    unit2 = input("Enter unit 2: ")
-    number *= c.convert(unit1, unit2)
-    print(f"Your result is {number} {unit2}")
+def changeUnits(*args):
+    units1["values"] = units[converters.get()]
+    units2["values"] = units[converters.get()]
 
-while True:
-    print("What converter do you need?")
-    print("1 - Distance")
-    print("2 - Mass")
-    print("3 - Time")
-    print("0 - Exit")
-    i = input("Your choice: ")
-    if i == "1":
-        dc = DistanceConverter()
-        convert(dc)
-    elif i == "2":
-        mc = MassConverter()
-        convert(mc)
-    elif i == "3":
-        tc = TimeConverter()
-        convert(tc)
-    elif i == "0":
-        print("Goodbye")
-        break
+def convert():
+    cur = converters.current()
+    if cur == 0:
+        con = DistanceConverter()
+    elif cur == 1:
+        con = MassConverter()
     else:
-        print("Wrong input")
+        con = TimeConverter()
+    number = float(entry.get())
+    unit1 = units1.get()
+    unit2 = units2.get()
+    number *= con.convert(unit1, unit2)
+    result["text"] = number
+
+units = {
+    "Distance": ["km", "m", "dm", "cm", "mm", "ml", "ft"],
+    "Mass": ["t", "kg", "g", "mg", "lb", "oz"],
+    "Time": ["y", "mon","d", "h", "min", "s", "ms"]
+}
+root = Tk()
+root.geometry("300x300")
+root.title("Converter")
+converters = ttk.Combobox(values=list(units.keys()), width=40)
+converters.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+entry = Entry(width=30)
+entry.grid(row=1, column=0, padx=5, pady=5)
+units1 = ttk.Combobox(width=10)
+units1.grid(row=1, column=1, padx=5, pady=5)
+units2 = ttk.Combobox(width=10)
+units2.grid(row=2, column=1, padx=5, pady=5)
+result = Label(text="...............")
+result.grid(row=2, column=0, padx=5, pady=5)
+button = Button(text="convert", width=30, command=convert)
+button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+converters.bind("<<ComboboxSelected>>", changeUnits)
+root.mainloop()
